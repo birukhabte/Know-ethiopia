ce31552dfba0d38ad1b9ffc7a1cef95bf0a7d2c2
biruk habte <biruk.habte-ug@aau.edu.et>
2025-02-26 10:04:04 +0300

    Minor content tweak for Ethiopia page

---
+++ know-ethiopia-main/frontend/src/pages/constitution/index.js
@@ -0,0 +1,8 @@
+export { default as PreamblePage } from './PreamblePage';
+export { default as ConstitutionOverview } from './ConstitutionOverview';
+export { default as ConstitutionalInitiation } from './ConstitutionalInitiation';
+export { default as AmendmentsPage } from './AmendmentsPage';
+export { default as KeyFeaturesPage } from './KeyFeaturesPage';
+
+
+// chore: know-ethiopia backfill 1774943307
